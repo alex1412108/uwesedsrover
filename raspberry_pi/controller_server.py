65ec89a6e308ee2aafeefa7d033c449387cd6f5e
Emmanuel Smith <esmith@projectaxil.us>
2018-06-30 19:53:32 +0100

    Add support for transmitting inputs to arduino

--- raspberry_pi/controller_server.py
+++ raspberry_pi/controller_server.py
@@ -3,22 +3,28 @@
 import inputs_pb2
 import socket
 import struct
-
+import serial
+SERIAL = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('', 8081))
-    server.listen(5)
-    while True:
-        connection, address = server.accept()
-        data_length = struct.unpack('H', connection.recv(2))[0]
-        data = connection.recv(data_length)
-        connection.close()
-        inputs = inputs_pb2.Input()
-        inputs.ParseFromString(data)
-        print(inputs)
-        input_bytes = serialise_inputs(inputs)
-        print(input_bytes)
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('', 8081))
+        server.listen(5)
+        SERIAL.open()
+        while True:
+            connection, address = server.accept()
+            data_length = struct.unpack('H', connection.recv(2))[0]
+            data = connection.recv(data_length)
+            connection.close()
+            inputs = inputs_pb2.Input()
+            inputs.ParseFromString(data)
+            print(inputs)
+            input_bytes = serialise_inputs(inputs)
+            print(input_bytes)
+            SERIAL.write(input_bytes)
+    except KeyboardInterrupt:
+        SERIAL.close()
 
 
 
